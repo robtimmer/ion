6377d18a1f37151b9200e5aa5a2eb3ebf3bd8f73
cevap <dev@i2pmail.org>
2019-01-20 19:37:35 +0100

    Add testnet checkpoint on 503266 and 514877 as last one

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -94,11 +94,13 @@ static Checkpoints::MapCheckpoints mapCheckpointsTestnet =
 	(  443601,      uint256("c5e9467f24b1d149a9716776be2fdc806af2db54d11330df33bb931f8ebf6254") ) // Testnet Fork June 2018
 	(  472999,      uint256("5d55f229074b930f4dd42a0f4e0e2f1d855dcd1d87cb86553ea21c797825482c") ) // Testnet Fork June 2018
 	(  473000,      uint256("237bc8d1e7cb02af8cd3e6affe1fa134c1a29a4852f3fa177ebcd50014463152") ) // Testnet Fork December 2018; timestamp=1545338100  txcount=947151
-	(  500535,      uint256("d6a5202217fcbdbe34592e78038937cb89541cb41051492bde6d061188285086") );// timestamp=1547025824 txcount=1003348
+	(  500535,      uint256("d6a5202217fcbdbe34592e78038937cb89541cb41051492bde6d061188285086") ) // timestamp=1547025824 txcount=1003348
+	(  503266,      uint256("6a80fba64c8db4cdd8568d7b99e0607d7862f43054e09c5d1dca2f784c2ef53b") )
+	(  514877,      uint256("3ab3d05a4a09b20f09ea9ff7797c7a35ab597ca6528bba08adc50207c0624386") ); // timestamp=1548000351 txcount=1032119
 static const Checkpoints::CCheckpointData dataTestnet = {
     &mapCheckpointsTestnet,
-    1547025824,
-    1003348,
+    1548000351,
+    1032119,
     2000};
 
 static Checkpoints::MapCheckpoints mapCheckpointsRegtest =
