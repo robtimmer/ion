a0df40c3038dafaab5b51c16668b7bc69e7945d1
cevap <dev@i2pmail.org>
2019-02-02 02:29:13 +0100

    Fix regtest style and showing testnet icons

--- src/qt/guiconstants.h
+++ src/qt/guiconstants.h
@@ -58,5 +58,6 @@ static const int MAX_URI_LENGTH = 255;
 #define QAPP_ORG_DOMAIN "ioncoin.org"
 #define QAPP_APP_NAME_DEFAULT "Ion-Qt"
 #define QAPP_APP_NAME_TESTNET "Ion-Qt-testnet"
+#define QAPP_APP_NAME_REGTEST "Ion-Qt-regtest"
 
 #endif // BITCOIN_QT_GUICONSTANTS_H
