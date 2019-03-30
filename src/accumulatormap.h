b12e0acc271354acd4860d2d2c3d6d36c1f9eb13
cevap <dev@i2pmail.org>
2019-03-30 06:30:50 +0100

    update accumulator and accumulator map

--- src/accumulatormap.h
+++ src/accumulatormap.h
@@ -2,27 +2,93 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef ION_ACCUMULATORMAP_H
-#define ION_ACCUMULATORMAP_H
+#ifndef ION_ACCUMULATORS_H
+#define ION_ACCUMULATORS_H
 
 #include "libzerocoin/Accumulator.h"
 #include "libzerocoin/Coin.h"
-#include "accumulatorcheckpoints.h"
+#include "libzerocoin/Denominations.h"
+#include "xion/zerocoin.h"
+#include "accumulatormap.h"
+#include "chain.h"
+#include "uint256.h"
+#include "bloom.h"
+#include "witness.h"
 
-//A map with an accumulator for each denomination
-class AccumulatorMap
-{
-private:
-    libzerocoin::ZerocoinParams* params;
-    std::map<libzerocoin::CoinDenomination, std::unique_ptr<libzerocoin::Accumulator> > mapAccumulators;
+class CBlockIndex;
+
+std::map<libzerocoin::CoinDenomination, int> GetMintMaturityHeight();
+
+/**
+ * Calculate the acc witness for a single coin.
+ * @return true if the witness was calculated well
+ */
+
+bool CalculateAccumulatorWitnessFor(
+        const libzerocoin::ZerocoinParams* params,
+        int startingHeight,
+        int maxCalculationRange,
+        libzerocoin::CoinDenomination den,
+        const CBloomFilter& filter,
+        libzerocoin::Accumulator& accumulator,
+        libzerocoin::AccumulatorWitness& witness,
+        int& nMintsAdded,
+        string& strError,
+        list<CBigNum>& ret,
+        int &heightStop
+);
+
+bool GenerateAccumulatorWitness(
+        const libzerocoin::PublicCoin &coin,
+        libzerocoin::Accumulator& accumulator,
+        libzerocoin::AccumulatorWitness& witness,
+        int& nMintsAdded,
+        string& strError,
+        CBlockIndex* pindexCheckpoint = nullptr);
+
+
+bool GenerateAccumulatorWitness(CoinWitnessData* coinWitness, AccumulatorMap& mapAccumulators, CBlockIndex* pindexCheckpoint);
+list<libzerocoin::PublicCoin> GetPubcoinFromBlock(const CBlockIndex* pindex);
+bool GetAccumulatorValueFromDB(uint256 nCheckpoint, libzerocoin::CoinDenomination denom, CBigNum& bnAccValue);
+bool GetAccumulatorValue(int& nHeight, const libzerocoin::CoinDenomination denom, CBigNum& bnAccValue);
+bool GetAccumulatorValueFromChecksum(uint32_t nChecksum, bool fMemoryOnly, CBigNum& bnAccValue);
+void AddAccumulatorChecksum(const uint32_t nChecksum, const CBigNum &bnValue, bool fMemoryOnly);
+bool CalculateAccumulatorCheckpoint(int nHeight, uint256& nCheckpoint, AccumulatorMap& mapAccumulators);
+void DatabaseChecksums(AccumulatorMap& mapAccumulators);
+bool LoadAccumulatorValuesFromDB(const uint256 nCheckpoint);
+bool EraseAccumulatorValues(const uint256& nCheckpointErase, const uint256& nCheckpointPrevious);
+uint32_t ParseChecksum(uint256 nChecksum, libzerocoin::CoinDenomination denomination);
+uint32_t GetChecksum(const CBigNum &bnValue);
+int GetChecksumHeight(uint32_t nChecksum, libzerocoin::CoinDenomination denomination);
+bool InvalidCheckpointRange(int nHeight);
+bool ValidateAccumulatorCheckpoint(const CBlock& block, CBlockIndex* pindex, AccumulatorMap& mapAccumulators);
+
+
+// Exceptions
+
+class NotEnoughMintsException : public std::exception {
 public:
-    explicit AccumulatorMap(libzerocoin::ZerocoinParams* params);
-    bool Load(uint256 nCheckpoint);
-    void Load(const AccumulatorCheckpoints::Checkpoint& checkpoint);
-    bool Accumulate(const libzerocoin::PublicCoin& pubCoin, bool fSkipValidation = false);
-    CBigNum GetValue(libzerocoin::CoinDenomination denom);
-    uint256 GetCheckpoint();
-    void Reset();
-    void Reset(libzerocoin::ZerocoinParams* params2);
+    std::string message;
+    NotEnoughMintsException(const string &message) : message(message) {}
 };
-#endif //ION_ACCUMULATORMAP_H
+
+class GetPubcoinException : public std::exception {
+public:
+    std::string message;
+    GetPubcoinException(const string &message) : message(message) {}
+};
+
+class ChecksumInDbNotFoundException : public std::exception {
+public:
+    std::string message;
+    ChecksumInDbNotFoundException(const string &message) : message(message) {}
+};
+
+class searchMintHeightException : public std::exception {
+public:
+    std::string message;
+    searchMintHeightException(const string &message) : message(message) {}
+};
+
+#endif //ION_ACCUMULATORS_H
+
