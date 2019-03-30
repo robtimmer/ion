c341fbb4af0cadaed232d92f1c0f270d704316bb
cevap <dev@i2pmail.org>
2019-03-31 00:28:29 +0100

    fix accumulatorcheckpoints.cpp
    after related zerocoin files were moved to src/xion folder,
    accumulatorcheckpoints.cpp had to be fixed, as it was changed
    wrongly to xion/.., because that file moved into the same folder

--- src/xion/accumulatorcheckpoints.cpp
+++ src/xion/accumulatorcheckpoints.cpp
@@ -3,7 +3,7 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include "xion/accumulatorcheckpoints.h"
-#include "xion/accumulatorcheckpoints.json.h"
+#include "accumulatorcheckpoints.json.h"
 
 namespace AccumulatorCheckpoints
 {
