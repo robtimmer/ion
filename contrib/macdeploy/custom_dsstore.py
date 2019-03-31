aab90e87e179f2f2c883e12cbdf6df915dc8448e
cevap <dev@i2pmail.org>
2019-03-31 04:41:49 +0200

    macos - rename Ion-Qt tp ION-qt
    we propose to change the name from Ion-Qt to ION-Qt on mac os

--- contrib/macdeploy/custom_dsstore.py
+++ contrib/macdeploy/custom_dsstore.py
@@ -53,7 +53,7 @@ ds['.']['icvp'] = icvp
 ds['.']['vSrn'] = ('long', 1)
 
 ds['Applications']['Iloc'] = (370, 156)
-ds['Ion-Qt.app']['Iloc'] = (128, 156)
+ds['ION-Qt.app']['Iloc'] = (128, 156)
 
 ds.flush()
 ds.close()
