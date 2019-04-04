35bd5ebda8c35e63691dfee84f122713a1ea76b3
cevap <dev@i2pmail.org>
2019-04-04 16:00:29 +0200

    serialization for all, not only nVersion 1
    - fixed python wrapped serials and other scripts using serialization
    
    fixes https://github.com/ioncoincore/ion/pull/141#issuecomment-479835386

--- src/primitives/transaction.h
+++ src/primitives/transaction.h
@@ -234,7 +234,7 @@ public:
     inline void SerializationOp(Stream& s, Operation ser_action, int nType, int nVersion) {
         READWRITE(*const_cast<int32_t*>(&this->nVersion));
         nVersion = this->nVersion;
-        if (nVersion == 1) READWRITE(*const_cast<uint32_t*>(&nTime));
+        READWRITE(*const_cast<uint32_t*>(&nTime));
         READWRITE(*const_cast<std::vector<CTxIn>*>(&vin));
         READWRITE(*const_cast<std::vector<CTxOut>*>(&vout));
         READWRITE(*const_cast<uint32_t*>(&nLockTime));
